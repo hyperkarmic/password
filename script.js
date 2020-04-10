67206036cffbd5987720a47c735041824af92630
hyperkarma <randy_pan@yahoo.com>
2020-04-10 15:44:14 +0100

    initial prompt box logic set up - including while loop to ensure that information is correct.  parseInt changes string to a number so it can be tested as a conditional statement

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Assignment Code
+var claw = prompt('Please enter a number between 8 and 128')
+var hood = parseInt(claw)
+var vrai
+
+if (hood > 7 && hood < 129) {vrai = true}else{vrai = false}
+while (vrai === false){
+  claw = prompt('Please enter a number between 8 and 128')
+  hood = parseInt(claw);
+  if (hood > 7 && hood < 129) {vrai = true}
+
+
+}
+ 
+
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
