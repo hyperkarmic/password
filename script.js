3d9fdfc6feef09a433097f690bf022cd4e9de5f1
hyperkarma <randy_pan@yahoo.com>
2020-04-12 13:03:40 +0100

    confirm prompts for character sets - set up.  Form validation while loop set up form confirm boxes, using while loop.  Password array of arrays is flattened and a variable is assigned to the length of this flattened array - for password generation purposes!

--- script.js
+++ script.js
@@ -1,16 +1,120 @@
 // Assignment Code
+var acc = 0;
+
+var password = [];
+let ranNum
+var abc = ["a","b","c"];
+var ABC = ["a","b","c"];
+var numbers = ["1","2","3"]
+var spesh = ["*","/","$"]
+
+//let ranNum
+
 var claw = prompt('Please enter a number between 8 and 128')
+// initial prompt ask
 var hood = parseInt(claw)
+//turns prompt response to numerical form
 var vrai
 
 if (hood > 7 && hood < 129) {vrai = true}else{vrai = false}
+//this logic sets up a condition for the while statement
 while (vrai === false){
+  //if the initial conditions aren't met the initial prompt is repeted
   claw = prompt('Please enter a number between 8 and 128')
   hood = parseInt(claw);
   if (hood > 7 && hood < 129) {vrai = true}
+  //once conditions for truth are met - while loop broken 
+
+  //now we are going to work on the prompts
+
+  
 
 
 }
+alert("You need to choose at least one of the following character types for your password!") 
+//this asks the question!!!!!
+
+
+
+//this adds one to the accumalator if confirmed.  and not if not.  ths will be repeated for each of the character types!
+if (confirm("Do you want to include lower case letters????")){
+  password.push(abc)
+
+}
+
+//confirm prompt 2
+
+if (confirm("Do you want to include upper case letters????")){
+  password.push(ABC)
+
+}
+
+//confirm prompt 3
+if (confirm("Do you want to include numbers case letters????")){
+  password.push(numbers)
+
+}
+
+//confirm prompt 4
+if (confirm("Do you want to include lower case letters????")){
+  acc++
+  password.push(spesh)
+
+}
+password = password.flat()
+//this turns array or arrays 'password' into a single array;
+//const statement is easy way of accessing array length for password generation purposes!!!
+
+ranNum = password.length;
+
+if (ranNum === 0){vrai = false}
+//boolean flag set up for form validation purposes
+
+while (vrai === false){
+  //if there has been no choice - the question repeats
+
+  alert("You need to choose at least one of the following character types for your password!") 
+//this asks the question - ***again***!!!!!
+
+
+
+//this adds one to the accumalator if confirmed.  and not if not.  ths will be repeated for each of the character types!
+if (confirm("Do you want to include lower case letters????")){
+  password.push(abc)
+
+}
+
+//confirm prompt 2
+
+if (confirm("Do you want to include upper case letters????")){
+  password.push(ABC)
+
+}
+
+//confirm prompt 3
+if (confirm("Do you want to include numbers case letters????")){
+  password.push(numbers)
+
+}
+
+//confirm prompt 4
+if (confirm("Do you want to include lower case letters????")){
+  acc++
+  password.push(spesh)
+
+}
+password = password.flat()
+//this turns array or arrays 'password' into a single array;
+//ranNum is easy way of accessing array length for password generation purposes!!!
+
+ranNum = password.length;
+if (ranNum > 0){vrai = true}
+}
+//resetting vrai to true breaks while loop - designating form validation!!!
+
+
+alert(ranNum)
+
  
 
 
